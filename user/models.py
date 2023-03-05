dbfa034c5e05a8a34f48bffb8c0eae45120e25cb
kwoshvick <kwoshvick@gmail.com>
2023-03-05 19:39:20 +0300

    user model & serializer and test

---
+++ user/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    national_id = models.IntegerField(null=False, blank=False)
+    birth_date = models.DateField(null=False, blank=False)
+    address = models.CharField(max_length=200, null=False, blank=False)
+    country = models.CharField(max_length=30, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    email = models.EmailField(max_length=30, null=False, blank=False)
+    finger_print_signature = models.CharField(max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.first_name} - {self.last_name}"
---
+++ user/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+
+urlpatterns = [
+    # path('', UsersView.as_view(), name='users'),
+]
