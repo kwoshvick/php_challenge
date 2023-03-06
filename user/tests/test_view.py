b95247b62dd1a0b63e11125a403ef59d38a9cb55
kwoshvick <kwoshvick@gmail.com>
2023-03-06 22:25:35 +0300

    test for view

--- user/tests/test_view.py
+++ user/tests/test_view.py
@@ -2,6 +2,10 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
+from io import BytesIO
+from rest_framework.parsers import MultiPartParser
+from rest_framework.test import APIRequestFactory, APITestCase
+from unittest.mock import patch
 from user.models import User
 
 
