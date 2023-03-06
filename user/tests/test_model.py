caa1b552aeb1374defee77036c8b71bda4ea1d6c
kwoshvick <kwoshvick@gmail.com>
2023-03-06 21:43:10 +0300

    fsm tests

--- user/tests/test_model.py
+++ user/tests/test_model.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from user.models import User
+from user.models import User, UserCsvFile
 
 
 class UserModelTest(TestCase):
@@ -19,3 +19,55 @@ class UserModelTest(TestCase):
 
     def test_user_count(self):
         self.assertEqual(1, User.objects.count())
+
+
+class UserCsvFileTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.file = UserCsvFile.objects.create(
+            name="234fg-gg.csv",
+            original_name="gg.csv",
+        )
+
+    # @classmethod
+    # def setUp(self):
+    #     self.file = UserCsvFile.objects.create(
+    #         name="234fg-gg.csv",
+    #         original_name="gg.csv",
+    #     )
+
+    def test_user_csv_file_count(self):
+        self.assertEqual(1, UserCsvFile.objects.count())
+
+    def test_default_state(self):
+        self.assertEqual("uploading", self.file.state)
+
+    def test_change_state_to_failed_from_uploading(self):
+        self.file.to_state_failed_from_uploading()
+        self.file.save()
+        self.assertEqual("failed", self.file.state)
+
+    def test_change_state_to_pending(self):
+        self.file.to_state_pending()
+        self.file.save()
+        self.assertEqual("pending", self.file.state)
+
+    def test_change_state_to_inserting(self):
+        self.file.to_state_pending()
+        self.file.to_state_inserting()
+        self.file.save()
+        self.assertEqual("inserting", self.file.state)
+
+    def test_change_state_to_failed_from_inserting(self):
+        self.file.to_state_pending()
+        self.file.to_state_inserting()
+        self.file.to_state_failed_from_inserting()
+        self.file.save()
+        self.assertEqual("failed", self.file.state)
+
+    def test_change_state_to_processed(self):
+        self.file.to_state_pending()
+        self.file.to_state_inserting()
+        self.file.to_state_processed()
+        self.file.save()
+        self.assertEqual("processed", self.file.state)
